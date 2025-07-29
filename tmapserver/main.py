676b2dc4b3a8bff5ed6555c049f096bfe2438638
Siven Panda <siven.panda@duke.edu>
2025-07-29 04:14:24 -0500

    forgot to save lol

--- tmapserver/main.py
+++ tmapserver/main.py
@@ -4,12 +4,15 @@ import anndata as ad
 
 def main():
     parser = argparse.ArgumentParser(description='Sanitize STHD files and spin up a TissUUmaps viewing server locally.')
-    parser.add_argument('adata_path', help='Path to save the enrichment analysis results CSV file.')
+    parser.add_argument('adata_path', help='Path to read adata object.')
+    parser.add_argument('output_path', help='Path to save sanitized adata object.')
     args = parser.parse_args()
 
     adata = ad.read_h5ad(args.adata_path)
 
     adata.obs.columns = [sanitize(c) for c in adata.obs.columns]
+    
+    adata.write_h5ad(args.output_path)
 
 if __name__ == "__main__":
     main()
